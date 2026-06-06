401f2837349dc4407bed647f95fb11964eee7ef5
NAYAN KRISHNA M <73845326+NayanKrishna13@users.noreply.github.com>
2026-06-07 01:14:57 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache={}
+        def findtarget(i,curr,target,nums):
+            if i==len(nums):
+                if curr==target:
+                    return 1
+                else:
+                    return 0
+            res=0
+            if (i,curr) in cache:
+                return cache[(i,curr)]
+            else:
+                res=findtarget(i+1,curr+nums[i],target,nums)+findtarget(i+1,curr-nums[i],target,nums)
+
+            cache[(i,curr)]=res
+
+            return res
+            
+        
+        return findtarget(0,0,target,nums)
+
+
+
