4a51b0e98a551d11985819911d0667aca6bc8edc
NAYAN KRISHNA M <73845326+NayanKrishna13@users.noreply.github.com>
2026-07-07 21:22:29 +0530

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.py
@@ -0,0 +1,21 @@
+
+class Solution:
+    def cloneGraph(self, node: "Node") -> "Node":
+        if not node:
+            return None
+        old_to_new = {node: Node(node.val)}
+        queue = deque([node])
+
+        while queue:
+            curr=queue.popleft()
+            for neighbor in curr.neighbors:
+                if neighbor not in old_to_new:
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                old_to_new[curr].neighbors.append(old_to_new[neighbor])
+
+        return old_to_new[node]
+
+
+        
+        
